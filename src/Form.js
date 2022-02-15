1159a067d2d4a42d68efe2266de1245e104b49bc
Mohammed Suhail <MSuhail@quinnox.com>
2022-02-15 20:05:17 +0530

    first commit

--- src/Form.js
+++ src/Form.js
@@ -4,28 +4,29 @@ class Form extends React.Component {
   render() {
     return (
       <div id="Form">
-        <h3>Customer Page:</h3>   
+        <h1>Customer Page:</h1> <br/><br/>  
         <form onSubmit={this.props.handleFormSubmit}>    
-          <label htmlFor="FirstName">
+          <label htmlFor="FirstName"><p>
           FirstName:
           <input id="FirstName" value={this.props.newFirstName} 
             type="text" required maxLength={10} onInput={this.maxLengthCheck} name="FirstName"
-            onChange={this.props.handleInputChange} />
-          </label><br/>
-          <label for="LastName">
+            onChange={this.props.handleInputChange} /></p>
+          </label><br/><br/>
+          <label for="LastName"><p>
           LastName:
           <input id="LastName" value={this.props.newLastName} 
             type="text" required maxLength={10} onInput={this.maxLengthCheck} name="LastName"
-            onChange={this.props.handleInputChange} />
-          </label><br/>
-          <label for="Age">
+            onChange={this.props.handleInputChange} /></p>
+          </label><br/><br/>
+          <label for="Age"><p>
           Age :    
           <input id="Age" value={this.props.newAge} 
             type="number" required min="1" max="100" name="Age"
-            onChange={this.props.handleInputChange} />
-          </label><br/>
-          <button type="submit" value="Submit">Add Item</button>
-          <button type="reset"  value="reset"onClick={this.props.resetForm}>Reset </button>
+            onChange={this.props.handleInputChange} /></p>
+          </label><br/><br/>
+          
+          <button type="submit" value="Submit">Add Item</button> &nbsp;&nbsp;&nbsp;
+          <button type="reset"  value="reset"onClick={this.props.resetForm}>Reset </button><br/><br/>
         </form>
       </div>
     );
